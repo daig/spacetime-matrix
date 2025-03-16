79c52e016668b9ad4388e1145b51aaaa33769743
Dai <daig@users.noreply.github.com>
2025-03-16 01:05:09 -0500

    cleanup

--- spacetime-matrix/spacetime_matrixApp.swift
+++ spacetime-matrix/spacetime_matrixApp.swift
@@ -11,16 +11,10 @@ import SwiftUI
 struct spacetime_matrixApp: App {
 
     @State private var appModel = AppModel()
-    @State private var avPlayerViewModel = AVPlayerViewModel()
 
     var body: some Scene {
         WindowGroup {
-            if avPlayerViewModel.isPlaying {
-                AVPlayerView(viewModel: avPlayerViewModel)
-            } else {
-                ContentView()
-                    .environment(appModel)
-            }
+            ContentView() .environment(appModel)
         }
 
         ImmersiveSpace(id: appModel.immersiveSpaceID) {
@@ -28,11 +22,9 @@ struct spacetime_matrixApp: App {
                 .environment(appModel)
                 .onAppear {
                     appModel.immersiveSpaceState = .open
-                    avPlayerViewModel.play()
                 }
                 .onDisappear {
                     appModel.immersiveSpaceState = .closed
-                    avPlayerViewModel.reset()
                 }
         }
         .immersionStyle(selection: .constant(.progressive), in: .progressive)
